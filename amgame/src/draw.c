2615af15e7512248dc0dce0add9c6b0affd68860
tracer-nju <tracer@nju.edu.cn>
2019-03-02 16:05:13 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 8156172534674346b3ce2c7e475e5c73     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  16:05:13 up  4:00,  5 users,  load average: 0.02, 0.12, 0.11

--- amgame/src/draw.c
+++ amgame/src/draw.c
@@ -1,8 +1,9 @@
 #include <game.h>
+#define W 128
+#define H 8
+extern char font8x8_basic[H][W];
 
-extern char font8x8_basic[128][8];
-
-static uint32_t gameboard[8][8]; //max word length:16
+static uint32_t gameboard[H][W]; //max word length:16
 
 static inline void draw_character(char ch, int x, int y, int color) {
   int i, j;
@@ -10,12 +11,13 @@ static inline void draw_character(char ch, int x, int y, int color) {
   for (i = 0; i < 8; i ++) 
     for (j = 0; j < 8; j ++) 
       if ((p[i] >> j) & 1)
-       // if (x + j < W && y + i < H)
+        if (x + j < W && y + i < H)
           gameboard[y + i][x + j] = color;
 }
 
 void redraw(){
 	draw_character('A',0,0,0xffffffff);
-	draw_rect(&gameboard[0][0],100,100,8,8);
+	draw_character('B',8,0,0xffff0000);
+	draw_rect(&gameboard[0][0],100,100,W,H);
 	draw_sync();
 }
