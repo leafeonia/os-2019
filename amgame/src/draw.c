eaadc1110d2d271fccd88f1e743017fcc190dae7
tracer-nju <tracer@nju.edu.cn>
2019-03-02 15:52:08 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 8156172534674346b3ce2c7e475e5c73     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  15:52:09 up  3:47,  5 users,  load average: 0.04, 0.11, 0.13

--- amgame/src/draw.c
+++ amgame/src/draw.c
@@ -2,6 +2,20 @@
 
 extern char font8x8_basic[128][8];
 
+static uint32_t gameboard[128][8]; //max word length:16
+
+static inline void draw_character(char ch, int x, int y, int color) {
+  int i, j;
+  char *p = font8x8_basic[(int)ch];
+  for (i = 0; i < 8; i ++) 
+    for (j = 0; j < 8; j ++) 
+      if ((p[i] >> j) & 1)
+       // if (x + j < W && y + i < H)
+          gameboard[y + i][x + j] = color;
+}
+
 void redraw(){
-	printf("FAFA");
+	draw_character('A',0,0,0xffffffff);
+	draw_rect(&gameboard[0][0],0,0,128,8);
+	draw_sync();
 }
