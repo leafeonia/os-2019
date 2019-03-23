1c70d545c51fc8b9ca61d89c536db05516385cbd
tracer-nju <tracer@nju.edu.cn>
2019-03-23 19:32:28 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: e8ee7c9b718c4df48504162b1b82840d     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  19:32:28 up  4:42,  4 users,  load average: 0.18, 0.17, 0.07

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -19,7 +19,7 @@ static void work_loop(void *arg) {
     for (int i = 0; i < 100; ++i) {
         printf("%s%d  ", s, get_count());
         add_count();
-        co_yield();
+        //co_yield();
     }
 }
 
