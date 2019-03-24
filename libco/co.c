5f7fa20a0adde4d3ff4c8208a078aafb77632edf
tracer-nju <tracer@nju.edu.cn>
2019-03-24 11:16:21 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: e8ee7c9b718c4df48504162b1b82840d     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  11:16:21 up  6:47,  4 users,  load average: 0.16, 0.13, 0.05

--- libco/co.c
+++ libco/co.c
@@ -14,7 +14,7 @@ void co_init() {
     s.current = -1;
     s.max_length = 0;
     s.valid = 0;
-    s.threads = (co*)malloc(sizeof(co)*NR_THREAD);
+    s.threads = (struct co*)malloc(sizeof(struct co)*NR_THREAD);
     getcontext(&(s.main));
     int i;
     for (i = 0; i < NR_THREAD; ++i) {
@@ -26,7 +26,7 @@ void transfer(){
 //    printf("transfer\n");
     if(s.current != -1){
         int id = s.current;
-        co* thread = &s.threads[id];
+        struct co* thread = &s.threads[id];
         thread->func(thread->arg);
         s.threads[id].state = FREE;
         s.current = -1;
@@ -45,7 +45,7 @@ struct co* co_start(const char *name, func_t func, void *arg) {
         printf("The number of threads is more than the design load. Please modify NR_THREAD(in co.h)\n");
         exit(1);
     }
-    co* t = &(s.threads[id]);
+    struct co* t = &(s.threads[id]);
     t->state = RUNNING;
     t->func = func;
     t->arg = arg;
@@ -89,7 +89,7 @@ void co_yield() {
     }
 }
 
-void co_wait(co *thd) {
+void co_wait(struct co *thd) {
     while(thd->state != FREE){
 //        setcontext(&(thd->context));
         s.current = thd->id;
