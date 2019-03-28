097d5b0f02a312bb1e4f5459c7e8c7e497f23b9b
tracer-nju <tracer@nju.edu.cn>
2019-03-28 22:54:47 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 77c6f3111ce4406fb446728135ada7b9     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  22:54:47 up  1:05,  2 users,  load average: 0.08, 0.09, 0.08

--- kernel/src/alloc.c
+++ kernel/src/alloc.c
@@ -5,17 +5,33 @@
 #define NAIVE
 
 static uintptr_t pm_start, pm_end;
+static pthread_mutex_t big_lock;
 
 static void pmm_init() {
   pm_start = (uintptr_t)_heap.start;
   pm_end   = (uintptr_t)_heap.end;
+  assert(pthread_mutex_init(&bigLock,NULL));
 }
 
 static void *kalloc(size_t size) {
+#ifdef NAIVE
+  pthread_mutex_lock(&big_lock);
+  void *ret = pm_start;
+  pm_start += size;  
+  pthread_mutex_unlock(&big_lock);
+  return ret;
+#else
+
+#endif
   return NULL;
 }
 
 static void kfree(void *ptr) {
+#ifdef NAIVE
+
+#else
+
+#endif
 }
 
 MODULE_DEF(pmm) {
