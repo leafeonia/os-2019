89634677a49ad9f7323220fdbc4db4a99a6be17a
tracer-nju <tracer@nju.edu.cn>
2019-03-31 14:31:56 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 77c6f3111ce4406fb446728135ada7b9     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  14:31:56 up  3:08,  2 users,  load average: 0.32, 0.07, 0.15

--- kernel/src/lock.c
+++ kernel/src/lock.c
@@ -7,11 +7,10 @@ void lock_init(lock_t *lock){
 
 void lock(lock_t *lock){
 	_intr_write(0);  //cli
-	log("enter\n");
 	while(_atomic_xchg(&lock->flag,1));
 }
 
 void unlock(lock_t *lock){
-	lock->flag = 0;
+	_atomic_xchg(&lock->flag,0);
 	_intr_write(1); //sti
 }
