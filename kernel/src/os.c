99b02d5454377837e06df07d2f2be0ec2dd90b4f
tracer-nju <tracer@nju.edu.cn>
2019-04-04 18:42:12 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 77c6f3111ce4406fb446728135ada7b9     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  18:42:12 up 10:34,  4 users,  load average: 1.37, 1.13, 0.58

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -72,11 +72,11 @@ void test_full(){
     }
     if(p_old != NULL){
       for(int i=0;i < 1000;i++){
-        //assert(p_old[i] == i);
-        if(p_old[i] != i){
+        assert(p_old[i] == i);
+        /*if(p_old[i] != i){
         	LOG("assertion fail");
         	return;
-        }
+        }*/
       }
     }
     //if(p_old != NULL)
