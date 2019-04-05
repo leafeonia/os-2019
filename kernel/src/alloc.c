afe2ae486fb56adbc699bb16ad4cc414bba37cba
tracer-nju <tracer@nju.edu.cn>
2019-04-05 11:43:01 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: ce9b02697e08450e9c1fb0e1565b55ef     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  11:43:01 up  5:10,  4 users,  load average: 0.29, 0.25, 0.33

--- kernel/src/alloc.c
+++ kernel/src/alloc.c
@@ -3,7 +3,7 @@
 #include <my_os.h>
 
 //#define NAIVE
-#define NALLOC 1024
+#define NALLOC 10240
 
 
 typedef long long ALIGN;
@@ -53,14 +53,14 @@ static void free(void* ap){
 	return;
 }
 
-static HEADER* morecore(size_t nunits){
+static void* morecore(size_t nunits){
 	if(nunits < NALLOC) nunits = NALLOC;
 	HEADER* ret = (HEADER*)pm_start;
 	if(pm_start + sizeof(HEADER)*nunits >= pm_end) return NULL;
 	pm_start += sizeof(HEADER)*nunits;
 	ret->s.size = nunits;
 	free((void*)(ret+1));
-	return freep;
+	return (void*)freep;
 }
 
 static void* fancy_alloc(size_t nbytes){
@@ -92,7 +92,7 @@ static void* fancy_alloc(size_t nbytes){
 			return (void*)(p+1);
 		}
 		if (p == freep){
-			if((p = morecore(nunits)) == NULL){
+			if((p = (HEADER*)morecore(nunits)) == NULL){
 				LOG("The heap is full");
 				return NULL;
 			}
