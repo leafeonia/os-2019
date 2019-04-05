db068f368cf83fbe44c2e471987b35da5cf4ca48
tracer-nju <tracer@nju.edu.cn>
2019-04-05 10:17:59 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: ce9b02697e08450e9c1fb0e1565b55ef     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  10:17:59 up  3:53,  4 users,  load average: 0.34, 0.41, 0.46

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -15,60 +15,15 @@ static void hello() {
   _putc("12345678"[_cpu()]); _putc('\n');
 }
 
-#define KALLOC_BLOCK 1024
-void test_big_small(){
-  int *p = NULL;
-  int *p_old = NULL;
-  size_t size[] = {
-    KALLOC_BLOCK/sizeof(int)/10*3,
-    KALLOC_BLOCK/sizeof(int)/10*1
-  };
-  int num = sizeof(size)/sizeof(size[0]);
-  for(int i=0;;i++){
-    if((p = pmm->alloc(size[i%num]*sizeof(int))) == NULL)
-      break;
-    //printf("test_big_small: start: %d\n", (uintptr_t)p);
-    //printf("\33[1;35mtest_big_small: I'm at %d, %d, %d\n\33[0m", (uintptr_t)p, i, _cpu()+1);
-    for(int j=0;j < size[i%num];j++){
-      p[j] = j;
-    }
-    
-    //printf("test_big_small: %d %d\n", (uintptr_t)p, (uintptr_t)p_old);
 
-    if(p_old != NULL){
-      for(int j=0;j < size[(i-1)%num];j++){
-        assert(p_old[j] == j);
-        /*if(p_old[j] != j){
-        	LOG("assertion fail");
-        	return;
-        }*/
-      }
-    }
-    
-    if(p_old != NULL)
-      pmm->free(p_old);
-    p_old = p;
-    /*
-    if(i >= 10)
-      break;
-    */
-  }
-}
-
-void test_full(){
-  //init_lock(&test_lock, 'b');
-  //printf("test_full: lock %p\n", &test_lock);
+void alloc_test(){
   int *p = NULL;
   int *p_old = NULL;
   int term = 0;
   while((p = pmm->alloc(1000*sizeof(int)))){
-    //spin_lock(&test_lock);
     //if((uintptr_t)p % 500 == 0)
     //printf("\33[1;35m malloc address:%x from cpu %d\n\33[0m", (uintptr_t)p,_cpu()+1);
-    //Assert(test_lock.slock == 1, "test_full: test_lock.slock值为0");
-    //spin_unlock(&test_lock);
     for(int i=0;i < 1000;i++){
-      //printf("test_full: I'm at %p, %d\n", p, i);
       p[i] = i;
     }
     if(p_old != NULL){
@@ -85,11 +40,6 @@ void test_full(){
       //printf("free %x\n",(uintptr_t)p);
     }
     p_old = p;
-    term++;
-    /*
-    if(term >= 5)
-      break;
-      */
   }
 }
 
@@ -109,66 +59,9 @@ static void alloc_test(){
 	printf("success~\n");
 } */
 
-#define SSIZE (1 << 12)
-#define MSIZE (1 << 30)
-#define MAXN 110
-#define TOFREE 0
-#define TOALLOC 1
-
-void test3(int n) {
-  /*更加随机的内存分配和回收*/
-  void* mmp[MAXN] = {};
-  int allocated[MAXN] = {};
-  srand(uptime());
-  memset(allocated, 0, sizeof(allocated));
-  int cnt = 0;
-  for(int i = 0; i < n; ++i) {
-    int flag = rand() % 2;
-    // acquire(&testlk);
-    if(flag == TOFREE && cnt > 0) {
-      // REDLog("#%d free, cnt=%d", i, cnt);
-      int id = 0;
-      while(allocated[id] == 0) {
-        id = rand() % n;
-        // REDLog("id=%d", id);
-      }
-      // REDLog("find id=%d", id);
-      pmm->free(mmp[id]);
-      allocated[id] = 0;
-      cnt--;
-    }
-    else {
-      size_t size = rand() %  MSIZE;
-      // PLog("Alloc Test size=0x%x", size);
-      void *ptr = pmm->alloc(size);
-      // PLog("Test #%d: Alloc pmm at %p, size=0x%x", i, ptr, size);
-      cnt ++;
-      // mmp[cnt] = ptr;
-      // allocated[cnt] = True;
-      for(int i = 0; i < n; ++i) {
-        if(!allocated[i]) {
-          mmp[i] = ptr;
-          allocated[i] = 1;
-          break;
-        }
-      }
-    }
-  }
-  // REDLog("Finish alloc");
-  for(int i = 0; i < n; ++i) {
-    // acquire(&testlk);
-    if(allocated[i]) {
-      pmm->free(mmp[i]);
-      allocated[i] = 0;
-    }
-  };
-  LOG("Test3 PASS!");
-}
 static void os_run() {
   hello();
-  //test_big_small();
-  test_full();
-  test3(100);
+  alloc_test();
   _intr_write(1);
   while (1) {
     _yield();
