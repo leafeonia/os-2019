89b002da59956e3bd062863fb962205ea985b134
tracer-nju <tracer@nju.edu.cn>
2019-04-05 10:02:27 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: ce9b02697e08450e9c1fb0e1565b55ef     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  10:02:27 up  3:38,  4 users,  load average: 0.29, 0.27, 0.13

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -109,48 +109,66 @@ static void alloc_test(){
 	printf("success~\n");
 } */
 
-void hyc_test() {
-//  spin_lock(&lk);
-  // TestLog("alloc_test begin...");
-  // TestLog("(1) Alloc memory for an array with 100 int...");
-  int *arr = (int*)pmm->alloc(100 * sizeof(int));
-  for (int i = 0; i < 100; i++){
-    arr[i] = i;
-  }
-  // TestLog("(1) Alloc done.");
-  for (int i = 0; i < 100; i++){
-    // TestLog("(1) arr[%d]: %d", i, arr[i]);
-  }
-  // TestLog("(2) Free memory for the array...");
-  pmm->free(arr);
-  // TestLog("(2) Free done.");
-  // TestLog("(3) Allocate lots of small memory...");
-  for (int i = 0; i < 100; i++){
-    pmm->alloc(1);
-    pmm->alloc(8);
-  }
-  // TestLog("(3) Small memory alloc done.");
-  // TestLog("(4) Allocate several large memory...");
-  pmm->alloc(1 << 12);
-  pmm->alloc(1 << 16);
-//  pmm->alloc(1 << 24);
-  // TestLog("(4) Large memory alloc done.");
-  // TestLog("(5) Frequent alloc and free...");
-  for (int i = 0; i < 100; i++){
-	void *addr = pmm->alloc(i);
-	pmm->free(addr);
-  }
-  // TestLog("(5) Frequent alloc and free done.");
-  // TestLog("alloc_test end.\n============================");
-//  spin_unlock(&lk);
-  LOG("hycTest PASS!");
-}
+#define SSIZE (1 << 12)
+#define MSIZE (1 << 30)
+#define MAXN 110
+#define TOFREE 0
+#define TOALLOC 1
 
+void test3(int n) {
+  /*更加随机的内存分配和回收*/
+  void* mmp[MAXN] = {};
+  bool allocated[MAXN] = {};
+  srand(uptime());
+  memset(allocated, 0, sizeof(allocated));
+  int cnt = 0;
+  for(int i = 0; i < n; ++i) {
+    int flag = rand() % 2;
+    // acquire(&testlk);
+    if(flag == TOFREE && cnt > 0) {
+      // REDLog("#%d free, cnt=%d", i, cnt);
+      int id = 0;
+      while(allocated[id] == False) {
+        id = rand() % n;
+        // REDLog("id=%d", id);
+      }
+      // REDLog("find id=%d", id);
+      pmm->free(mmp[id]);
+      allocated[id] = False;
+      cnt--;
+    }
+    else {
+      size_t size = rand() %  MSIZE;
+      // PLog("Alloc Test size=0x%x", size);
+      void *ptr = pmm->alloc(size);
+      // PLog("Test #%d: Alloc pmm at %p, size=0x%x", i, ptr, size);
+      cnt ++;
+      // mmp[cnt] = ptr;
+      // allocated[cnt] = True;
+      for(int i = 0; i < n; ++i) {
+        if(!allocated[i]) {
+          mmp[i] = ptr;
+          allocated[i] = True;
+          break;
+        }
+      }
+    }
+  }
+  // REDLog("Finish alloc");
+  for(int i = 0; i < n; ++i) {
+    // acquire(&testlk);
+    if(allocated[i]) {
+      pmm->free(mmp[i]);
+      allocated[i] = False;
+    }
+  };
+  LOG("Test3 PASS!");
+}
 static void os_run() {
   hello();
   //test_big_small();
-  //test_full();
-  hyc_test();
+  test_full();
+  test3(100);
   _intr_write(1);
   while (1) {
     _yield();
