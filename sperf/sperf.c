bbd234f6ebb019f0e6a6aa5e526da085ee6548ce
tracer-nju <tracer@nju.edu.cn>
2019-04-10 20:40:28 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: ce9b02697e08450e9c1fb0e1565b55ef     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  20:40:28 up  8:36,  4 users,  load average: 0.07, 0.02, 0.00

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -8,8 +8,6 @@
 
 int main(int argc, char *argv[]) {
 	printf("FUCK\n");
-	execlp("strace","strace","-T","ls",NULL);
-	/*
 	int fd[2];
 	if(pipe(fd) == -1) ERR("pipe fails");
   	int rc = fork();
@@ -19,12 +17,12 @@ int main(int argc, char *argv[]) {
   	else if(rc == 0){ //child
   		close(fd[0]);
   		dup2(fd[1],STDOUT_FILENO);
-  		execl("strace","strace","-T","ls",NULL);
+  		execlp("strace","strace","-T","ls",NULL);
   	}
   	else{
   		close(fd[1]); //close stdout, only read in
   		dup2(STDOUT_FILENO,fd[0]);
   	}
-  	*/
+  	
     return 0;
 }
