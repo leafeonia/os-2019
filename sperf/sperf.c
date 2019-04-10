9ec74fa9b7bb68f59b5f6b80b777361c381df27e
tracer-nju <tracer@nju.edu.cn>
2019-04-10 21:31:21 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: ce9b02697e08450e9c1fb0e1565b55ef     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  21:31:21 up  9:26,  4 users,  load average: 0.05, 0.10, 0.05

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -3,6 +3,7 @@
 #include<stdlib.h>
 #include<fcntl.h>
 #include<sys/wait.h>
+#include<assert.h>
 #define LOG(s) printf("\33[1;35m%s\n\33[0m",s)
 
 #define ERR(s)\
@@ -18,12 +19,12 @@ int main(int argc, char *argv[]) {
   		ERR("fork fails");
   	}
   	else if(rc == 0){ //child
-  		
+  		LOG("FUCK FROM CHILD");
   		close(fd[0]);
   		dup2(fd[1],STDOUT_FILENO);
   		//execlp("strace","strace","-T","ls",NULL);
   		execlp("ls","ls",NULL);
-  		LOG("FUCK FROM CHILD");
+  		assert(0);
   	}
   	else{
   		close(fd[1]); //close stdout, only read in
