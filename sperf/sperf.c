b1b792b93e937b5671daa63dd04a434b60886235
tracer-nju <tracer@nju.edu.cn>
2019-04-10 20:41:53 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: ce9b02697e08450e9c1fb0e1565b55ef     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  20:41:53 up  8:37,  4 users,  load average: 0.19, 0.06, 0.01

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -16,11 +16,13 @@ int main(int argc, char *argv[]) {
   		ERR("fork fails");
   	}
   	else if(rc == 0){ //child
+  		printf("FUCK FROM CHILD");
   		close(fd[0]);
   		dup2(fd[1],STDOUT_FILENO);
   		execlp("strace","strace","-T","ls",NULL);
   	}
   	else{
+  		  		printf("FUCK FROM PARENT");
   		close(fd[1]); //close stdout, only read in
   		dup2(STDOUT_FILENO,fd[0]);
   	}
