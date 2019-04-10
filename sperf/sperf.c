89e55d9d7bda009747be8adb8985fc46aa6fc0b6
tracer-nju <tracer@nju.edu.cn>
2019-04-10 21:09:11 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: ce9b02697e08450e9c1fb0e1565b55ef     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  21:09:11 up  9:04,  4 users,  load average: 0.15, 0.10, 0.02

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -3,6 +3,7 @@
 #include<stdlib.h>
 #include<fcntl.h>
 #include<sys/wait.h>
+#define LOG(s) printf("\33[0;31m%s\n\33[0m",s)
 
 #define ERR(s)\
 	printf("error: %s\n",s);\
@@ -12,20 +13,20 @@ int main(int argc, char *argv[]) {
 	pid_t rc;
 	int fd[2];
 	pipe(fd);
-	if(pipe(fd)) ERR("pipe fails");
-	printf("FUCK\n");
+	//if(pipe(fd)) ERR("pipe fails");
+	//printf("FUCK\n");
   	rc = fork();
   	if(rc < 0){
   		ERR("fork fails");
   	}
   	else if(rc == 0){ //child
-  		printf("FUCK FROM CHILD");
+  		LOG("FUCK FROM CHILD");
   		close(fd[0]);
   		dup2(fd[1],STDOUT_FILENO);
   		execlp("strace","strace","-T","ls",NULL);
   	}
   	else{
-  		  		printf("FUCK FROM PARENT");
+  		LOG("FUCK FROM PARENT");
   		close(fd[1]); //close stdout, only read in
   		dup2(STDOUT_FILENO,fd[0]);
   	}
