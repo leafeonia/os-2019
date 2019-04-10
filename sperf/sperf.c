89c5c8be6273c5d121c0b52ac1ce9297cd472e31
tracer-nju <tracer@nju.edu.cn>
2019-04-10 21:40:36 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: ce9b02697e08450e9c1fb0e1565b55ef     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  21:40:36 up  9:36,  4 users,  load average: 0.09, 0.09, 0.06

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -30,8 +30,11 @@ int main(int argc, char *argv[]) {
   		close(fd[1]); //close stdout, only read in
   		dup2(fd[0],STDIN_FILENO);
   		char s[1000000] = "";
-  		while(scanf("%s",s))
+  		for(int i = 0;i < 5;i++){
+  			scanf("%s",s));
   			printf("%s\n",s);
+  		}
+  		
   		LOG("FUCK FROM PARENT");
   	}
   	
