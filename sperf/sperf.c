1ec96c556901ea87d78bb1e7972a958d3c2c93d5
tracer-nju <tracer@nju.edu.cn>
2019-04-11 20:01:10 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: c1cd72e2516e42e6bb23e30b89c3b877     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  20:01:10 up  2:03,  4 users,  load average: 0.23, 0.13, 0.08

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -39,21 +39,22 @@ int main(int argc, char *argv[]) {
   		dup2(fd[0],STDIN_FILENO);
   		//LOG("FUCK FROM PARENT");
   		char buf[1024];
-  		regex_t preg_one;//, preg_two;//match syscall name, time, perspectively
-  		regmatch_t matches_one[1];//,matches_two[1];
+  		regex_t preg_one, preg_two;//match syscall name, time, perspectively
+  		regmatch_t matches_one[1],matches_two[1];
   		regcomp(&preg_one,"^[a-zA-Z]+",REG_EXTENDED);
+  		regcomp(&preg_two,"<.*>",REG_EXTENDED);
   		while(fgets(buf,1024,stdin)){
-  			int is_matched = regexec(&preg_one,buf,1,matches_one,0);
-  			if(is_matched == REG_NOMATCH){
+  			int is_matched_one = regexec(&preg_one,buf,1,matches_one,0);
+  			int is_matched_two = regexec(&preg_two,buf,1,matches_two,0);
+  			if(is_matched_one == REG_NOMATCH || is_matched_two == REG_NOMATCH){
   				printf("NO MATCH\n");
-  			}
-  				
+  			}	
   			else{
-  				printf("enter\n");
-  				char sysname[LEN_NAME];
-  				memcpy(sysname,buf+matches_one[0].rm_so,matches_one[0].rm_eo-matches_one[0].rm_so);
-  				sysname[matches_one[0].rm_eo-matches_one[0].rm_so] = '\0';
-  				printf("%s\n",sysname);
+  				char sys_name[LEN_NAME],sys_time[10];
+  				memcpy(sys_name,buf+matches_one[0].rm_so,matches_one[0].rm_eo-matches_one[0].rm_so);
+  				memcpy(sys_time,buf+matches_two[0].rm_so,matches_two[0].rm_eo-matches_two[0].rm_so);
+  				sys_name[matches_two[0].rm_eo-matches_two[0].rm_so] = '\0';
+  				printf("%s %f\n",sys_name,atof(sys_time));
   			}
   			//printf("%s",buf);	
   		}
