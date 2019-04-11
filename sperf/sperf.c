81d590df7cdaf3a97c1e8a4b8e179b06dd4c9986
tracer-nju <tracer@nju.edu.cn>
2019-04-11 20:25:02 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: c1cd72e2516e42e6bb23e30b89c3b877     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  20:25:02 up  2:27,  4 users,  load average: 0.05, 0.08, 0.05

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -56,6 +56,7 @@ int main(int argc, char *argv[]) {
   				memcpy(sys_name,buf+matches_one[0].rm_so,matches_one[0].rm_eo-matches_one[0].rm_so);
   				memcpy(sys_time,buf+matches_two[0].rm_so,matches_two[0].rm_eo-matches_two[0].rm_so);
   				sys_name[matches_one[0].rm_eo-matches_one[0].rm_so] = '\0';
+  				sys_time[matches_two[0].rm_eo-matches_two[0].rm_so] = '\0';
   				printf("%s %s\n",sys_name,sys_time);
   			}
   			//printf("%s",buf);	
