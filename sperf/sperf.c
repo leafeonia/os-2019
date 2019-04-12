b8ba9ccf9ecf0889f1dafdac44b9f3abb81c2583
tracer-nju <tracer@nju.edu.cn>
2019-04-12 22:53:09 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: c1cd72e2516e42e6bb23e30b89c3b877     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  22:53:09 up  7:41,  5 users,  load average: 1.08, 1.12, 0.93

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -79,8 +79,10 @@ void update(){
   		used += list[i].sys_time;
   		double ratio = list[i].sys_time/tot;
   		printf("%s %.02f%%",list[i].sys_name,ratio*100);
-  		printf("%s",blank(max_len - 4 - strlen(list[i].sys_name)) + (ratio >= 0.1));
-  		printf("%s%s%s\n",color[color_id],blank((int)(LEN_STICK*ratio)+1),normal);
+  		blank((max_len - 4 - strlen(list[i].sys_name)) + (ratio >= 0.1));
+  		PRINT(color_id);
+  		blank((int)(LEN_STICK*ratio)+1);
+  		NORMALIZE();
   	}
   	if(tot > used){
   		printf("others: %.02f%%",100 - used/tot*100);
