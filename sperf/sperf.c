a93ba313519aec2ad6b9490f67291b4d4e71f877
tracer-nju <tracer@nju.edu.cn>
2019-04-12 19:15:01 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: c1cd72e2516e42e6bb23e30b89c3b877     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  19:15:01 up  4:50,  4 users,  load average: 0.27, 0.19, 0.11

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -81,7 +81,9 @@ int main(int argc, char *argv[]) {
   		dup2(fd[1],STDERR_FILENO);
   		int devnull = open("/dev/null",O_WRONLY);
  		dup2(devnull,STDOUT_FILENO);
-  		execlp("strace","strace","-T","-e","raw=all","python","-c","2**1000",NULL);
+  		//execlp("strace","strace","-T","-e","raw=all","python","-c","2**1000",NULL);
+  		char * argv[] = {"ls", "-al", "/etc/passwd", 0};
+    	execvp("ls", argv);
   		char* tmp[] = {"strace","ls",0}; 
   		execvp("strace",tmp);
   		//execlp("ls","ls",NULL);
