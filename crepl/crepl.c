23df0d3314b6c1bd29c16e25930e509c64aca4dd
tracer-nju <tracer@nju.edu.cn>
2019-04-13 17:06:26 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 4b012a419bef4f0085da1e2640e28654     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  17:06:26 up  1:20,  4 users,  load average: 0.02, 0.01, 0.00

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -2,12 +2,27 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<sys/unistd.h>
+#include<sys/wait.h>
 #include<string.h>
+#include<assert.h>
 
 #define ERR(s)\
 	{printf("error: %s\n",s);\
 	exit(1);}\
-	
+
+void upload_so(char* source_name){
+	int rc = fork();
+	if(rc < 0) ERR("fork fails");
+	else if(rc == 0){
+		execlp("gcc","gcc","-fPIC","-shared",source_name,"-o"," libfunc.so");
+		assert(0);
+	}
+	else{
+		int wc = wait(NULL);
+	}
+	return 0;
+} 
+
 int main(int argc, char *argv[]) {
     char template[] = "temp-XXXXXX.c";
     int fd = mkstemps(template,2);
@@ -21,6 +36,7 @@ int main(int argc, char *argv[]) {
     	if(strcmp(command,"\n") == 0) continue;
     	if(strcmp(command,"q\n") == 0) break;
     	if(write(fd, command, strlen(command)) == -1) ERR("write fails");
+    	upload_so(template);
     	printf(">> ");
     }
     //read(fd,command,sizeof(command));
