7685aa167fefa0c6e8cb44c9c19824420c2df296
tracer-nju <tracer@nju.edu.cn>
2019-04-13 16:38:52 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 4b012a419bef4f0085da1e2640e28654     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  16:38:52 up 52 min,  4 users,  load average: 0.00, 0.00, 0.00

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[]) {
     while(1){
     	memset(command,0,sizeof(command));
     	fgets(command,sizeof(command),stdin);
-    	if(strcmp(command,"q") == 0) break;
+    	if(strcmp(command,"q\n") == 0) break;
     	if(write(fd, command, strlen(command)) == -1) ERR("write fails");
     }
     read(fd,command,sizeof(command));
