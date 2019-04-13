eb09ad8078e7d9b5ae3b9733bdc82b6ab1ed19e0
tracer-nju <tracer@nju.edu.cn>
2019-04-13 16:01:31 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 4b012a419bef4f0085da1e2640e28654     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  16:01:31 up 15 min,  4 users,  load average: 0.06, 0.05, 0.06

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -12,6 +12,14 @@ int main(int argc, char *argv[]) {
     int fd = mkstemps(template,2);
     if (fd == -1) ERR("mkstemp fails");
     printf("template=%s,fd = %d\n", template, fd); 
+    char command[512];
+    while(1){
+    	scanf("%s",command);
+    	if(strcmp(command,"q") == 0) break;
+    	if(write(fd, command, strlen(command)) == -1) ERR("write fails");
+    }
+    read(fd,command,sizeof(command));
+    printf("%s\n",command);
     unlink(template);
     close(fd);
     return 0;
