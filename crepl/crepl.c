168af680a40d86b59103fdf06549964da07845d4
tracer-nju <tracer@nju.edu.cn>
2019-04-13 15:54:52 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 4b012a419bef4f0085da1e2640e28654     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  15:54:52 up 8 min,  4 users,  load average: 0.20, 0.16, 0.10

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -4,13 +4,13 @@
 #include <sys/unistd.h>
 
 #define ERR(s)\
-	printf("error: %s\n",s);\
-	exit(1);\
+	{printf("error: %s\n",s);\
+	exit(1);}\
 	
 int main(int argc, char *argv[]) {
     char template[] = "template-XXXXXX.c";
     int fd = mkstemps(template,2);
-    if (fd == -1) ERR("mkstemp fails");
+    if (fd == -1) printf("FUCK");//ERR("mkstemp fails");
     printf("template=%s,fd = %d\n", template, fd); 
     unlink(template);
     close(fd);
