2655fbe55fe24673ac1af780a385a8926b56ad13
tracer-nju <tracer@nju.edu.cn>
2019-04-14 11:04:57 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 4b012a419bef4f0085da1e2640e28654     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  11:04:57 up  5:05,  4 users,  load average: 0.03, 0.11, 0.07

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -32,7 +32,7 @@ void upload_so(char* source_name,char* lib_name,int command_len){
 			//printf("command len = %d\n",command_len);
 			fseek(fp,-2,SEEK_END);
 			printf("%ld\n",ftell(fp));
-			fputc('/',fp);
+			assert(fputs("//",fp) != EOF)
 		}
 		/*close(fd[1]); 
   		dup2(fd[0],STDIN_FILENO);
