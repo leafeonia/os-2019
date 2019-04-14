d20922b1074898de6fa3252ec7e86579c53e7f7e
tracer-nju <tracer@nju.edu.cn>
2019-04-14 10:55:54 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 4b012a419bef4f0085da1e2640e28654     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  10:55:54 up  4:56,  4 users,  load average: 0.40, 0.11, 0.04

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -16,8 +16,8 @@ void upload_so(char* source_name,char* lib_name,int command_len){
 	
 	if(rc < 0) ERR("fork fails");
 	if(rc == 0){
-		//int devnull = open("/dev/null",O_WRONLY);
- 		//dup2(devnull,STDERR_FILENO);
+		int devnull = open("/dev/null",O_WRONLY);
+ 		dup2(devnull,STDERR_FILENO);
 		execlp("gcc","gcc","-fPIC","-shared",source_name,"-o",lib_name,NULL);
 		assert(0);
 	}
