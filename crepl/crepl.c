d5daec0e23971887256a305e2fc038fcaa6d391e
tracer-nju <tracer@nju.edu.cn>
2019-04-14 17:21:31 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 4b012a419bef4f0085da1e2640e28654     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  17:21:31 up 11:21,  4 users,  load average: 0.33, 0.10, 0.02

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -100,9 +100,9 @@ int main(int argc, char *argv[]) {
     		if((handle = dlopen(lib_name,RTLD_LAZY)) == NULL) {
     			printf("%s\n",dlerror());
     			ERR("dlopen fails");
-    			
     		}
     		char func_name[20];
+    		printf("expr_id = %d\n",expr_id);
     		sprintf(func_name,"__expr_wrap_%d",expr_id);
     		int (*func)() = dlsym(handle,func_name);
     		if(dlerror() != NULL) printf("%s\n",dlerror());
---
+++ crepl/temp-l3IMqD.c
@@ -0,0 +1,4 @@
+int f(){return 1;}
+int __expr_wrap_1() {return f();}
+int g(){return 2;}
+int __expr_wrap_2() {return g();}
