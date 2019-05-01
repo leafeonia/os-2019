2abec4d72a1e68f60e29e8bb486824448db99c1d
tracer-nju <tracer@nju.edu.cn>
2019-05-01 23:10:43 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 4168da8be8ec47a58693887131631321     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  23:10:44 up  5:23,  4 users,  load average: 0.01, 0.13, 0.13

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -10,6 +10,7 @@ void echo_task(void *name){
 	device_t *tty = dev_lookup(name);
 	while(1){
 		char line[128], text[128];
+		LOG(name);
 		sprintf(text, "(%s) $ ",name);
 		tty_write(tty, 0, text, strlen(name));
 		int nread = tty->ops->read(tty, 0, line, sizeof(line));
