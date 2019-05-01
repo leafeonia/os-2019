c6fe0f684b475a9aec2eb034782436c5736f3915
tracer-nju <tracer@nju.edu.cn>
2019-05-01 22:58:09 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 4168da8be8ec47a58693887131631321     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  22:58:09 up  5:10,  4 users,  load average: 0.07, 0.06, 0.07

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -4,7 +4,12 @@
 #include <kernel.h>
 #include <nanos.h>
 
-struct task {};
+struct task {
+	int id;
+	uint8_t fence1[32];
+	uint8_t stack[4096];
+	uint8_t fence2[32];
+};
 struct spinlock {};
 struct semaphore {};
 
