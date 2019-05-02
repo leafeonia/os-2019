352acbab2dbcd4093e0da56e91e09edcb55114ef
tracer-nju <tracer@nju.edu.cn>
2019-05-02 23:13:17 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 4168da8be8ec47a58693887131631321     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  23:13:17 up 11:15,  4 users,  load average: 0.24, 0.32, 0.20

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -34,7 +34,7 @@ static int kmt_create(task_t *task, const char *name, void (*entry)(void *arg), 
 	_Area stack = (_Area){task->stack, task->fence2};
 	task->context = *_kcontext(stack, entry, arg);
 	current = task;
-	printf("&current->context.eip = 0x%x\n",&current->context.eip);
+	printf("current->context.eip = 0x%x\n",current->context.eip);
 	printf("func_entry = 0x%x\n",entry);
 	return 0;
 }
