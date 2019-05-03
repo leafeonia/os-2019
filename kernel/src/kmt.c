613569827daa06b8a36cdf171482d96731ef1aad
tracer-nju <tracer@nju.edu.cn>
2019-05-04 00:13:02 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 4168da8be8ec47a58693887131631321     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  00:13:02 up 20:41,  4 users,  load average: 0.09, 0.19, 0.16

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -104,16 +104,17 @@ static _Context* kmt_context_save(_Event ev, _Context *ctx){
 }
 
 static _Context* kmt_context_switch(_Event ev, _Context *ctx){
-	
+	kmt_spin_lock(&lk_kmt_switch);
 	//printf("intr_read = %d\n",_intr_read());
 	//printf("outside: intr_read = %d\n",_intr_read());
-	if(*current) assert((*current)->fence1 == MAGIC1 && (*current)->fence2 == MAGIC2);
+	if(!(*current)) return NULL;
+	else assert((*current)->fence1 == MAGIC1 && (*current)->fence2 == MAGIC2);
 	//LOG("kmt_context_switch");
 	//printf("ctx = 0x%x\n",ctx);
-	kmt_spin_lock(&lk_kmt_switch);
+	
 	kmt_spin_unlock(&lk_kmt_switch);
-	task_t* cur_deref = *current;
-	if(!cur_deref) return NULL;
+	//task_t* cur_deref = *current;
+	//if(!cur_deref) return NULL;
 	
 	//printf("tasks[0] = 0x%x, &tasks[0] = 0x%x, tasks[1] = 0x%x, &tasks[1] = 0x%x\n", tasks[0], &tasks[0], tasks[1], &tasks[1]);
 	
