8a79e16ee986b299bd2f3e047a9d0887b0711d70
tracer-nju <tracer@nju.edu.cn>
2019-05-03 18:03:42 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 4168da8be8ec47a58693887131631321     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  18:03:42 up 16:30,  4 users,  load average: 0.04, 0.11, 0.15

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -23,6 +23,10 @@ static task_t **current;
 	(&dummy1)	(&dummy2)
 */
 
+static _Context* kmt_context_save(_Event ev, _Context *ctx){
+	(*current)->context = *ctx;
+	return NULL;
+}
 
 static _Context* kmt_context_switch(_Event ev, _Context *ctx){
 	//LOG("kmt_context_switch");
@@ -50,7 +54,7 @@ static void kmt_init(){
 	//LOG("kmt_init");
 	//printf("tasks[0] = 0x%x, &tasks[0] = 0x%x, tasks[1] = 0x%x, &tasks[1] = 0x%x\n", tasks[0], &tasks[0], tasks[1], &tasks[1]);
 	current = tasks;
-	
+	os->on_irq(INT_MIN, _EVENT_NULL, kmt_context_save);
 	os->on_irq(INT_MAX, _EVENT_NULL, kmt_context_switch);
 	/*for(int i = 0;i < NR_TASK;i++){
 		task_t* task = &tasks[i];
