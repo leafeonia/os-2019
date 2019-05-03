6d249f57330f84ffe7c09b8fae3098abb6d67653
tracer-nju <tracer@nju.edu.cn>
2019-05-03 21:21:47 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 4168da8be8ec47a58693887131631321     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  21:21:47 up 17:50,  4 users,  load average: 0.15, 0.08, 0.07

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -51,7 +51,7 @@ static _Context* kmt_context_switch(_Event ev, _Context *ctx){
 	//cur_deref->context = *ctx;
 	//LOG("checkpoint 1");
 	//printf("cur_deref + 1 = 0x%x, ")
-	printf("KEY: 0x%x\n",&tasks);
+	printf("KEY: 0x%x\n",tasks);
 	if(current + 1 == &tasks[task_id]){
 		//LOG("checkpoint 2");
 		current = &tasks[0];
