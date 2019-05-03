ca26e32329c07cea153b441e06ea2c743fce9f56
tracer-nju <tracer@nju.edu.cn>
2019-05-03 23:45:24 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 4168da8be8ec47a58693887131631321     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  23:45:24 up 20:14,  4 users,  load average: 0.27, 0.11, 0.08

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -85,7 +85,7 @@ static _Context* kmt_context_save(_Event ev, _Context *ctx){
 	//LOG("enter kmt_context_save");
 	if(*current) {
 		//printf("*current = 0x%x\n",*current);
-		//(*current)->context = *ctx;
+		(*current)->context = *ctx;
 	}
 	//printf("AFTER :ctx->eip = 0x%x, *current = 0x%x, &tasks[0] = 0x%x, tasks[0]->context.eip = 0x%x, &tasks[1] = 0x%x, tasks[1]->context.eip = 0x%x\n\n\n",ctx->eip, *current, &tasks[0], tasks[0]->context.eip, &tasks[1], tasks[1]->context.eip);
 	return NULL;
