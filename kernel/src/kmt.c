9f63f4049a5b2d749ee66e3cf01cf90d9a00bd49
tracer-nju <tracer@nju.edu.cn>
2019-05-03 20:32:03 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 4168da8be8ec47a58693887131631321     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  20:32:03 up 17:00,  4 users,  load average: 0.22, 0.13, 0.10

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -25,7 +25,7 @@ static task_t **current;
 
 static _Context* kmt_context_save(_Event ev, _Context *ctx){
 	if(*current) assert((*current)->fence1 == MAGIC1 && (*current)->fence2 == MAGIC2);
-	//if(*current) (*current)->context = *ctx;
+	if(*current) (*current)->context = *ctx;
 	return NULL;
 }
 
