443a5ba3d787a820107f5da6990becd2a8938925
tracer-nju <tracer@nju.edu.cn>
2019-05-03 10:28:16 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 4168da8be8ec47a58693887131631321     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  10:28:16 up 12:58,  4 users,  load average: 0.05, 0.17, 0.24

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -50,8 +50,9 @@ static _Context* kmt_context_switch(_Event ev, _Context *ctx){
 static void kmt_init(){
 	LOG("kmt_init");
 	printf("tasks[0] = 0x%x, &tasks[0] = 0x%x, tasks[1] = 0x%x, &tasks[1] = 0x%x\n", tasks[0], &tasks[0], tasks[1], &tasks[1]);
-	os->on_irq(INT_MAX, _EVENT_NULL, kmt_context_switch);
 	*current = NULL;
+	os->on_irq(INT_MAX, _EVENT_NULL, kmt_context_switch);
+	
 	/*for(int i = 0;i < NR_TASK;i++){
 		task_t* task = &tasks[i];
 		_Area stack = (_Area){task->stack,task->fence2};
