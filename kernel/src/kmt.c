f2e27aa75925775d5fc3d6e1073dd54725f30d27
tracer-nju <tracer@nju.edu.cn>
2019-05-04 00:16:30 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 4168da8be8ec47a58693887131631321     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  00:16:30 up 20:45,  4 users,  load average: 0.43, 0.28, 0.20

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -90,10 +90,11 @@ static void kmt_sem_signal(sem_t *sem){
 */
 
 static _Context* kmt_context_save(_Event ev, _Context *ctx){
-	kmt_spin_lock(&lk_kmt_save);
+	
 	if(*current) assert((*current)->fence1 == MAGIC1 && (*current)->fence2 == MAGIC2);
 	//printf("BEFORE:ctx->eip = 0x%x, *current = 0x%x, &tasks[0] = 0x%x, tasks[0]->context.eip = 0x%x, &tasks[1] = 0x%x, tasks[1]->context.eip = 0x%x\n\n",ctx->eip, *current, &tasks[0], tasks[0]->context.eip, &tasks[1], tasks[1]->context.eip);
 	//LOG("enter kmt_context_save");
+	kmt_spin_lock(&lk_kmt_save);
 	if(*current) {
 		//printf("*current = 0x%x\n",*current);
 		(*current)->context = *ctx;
