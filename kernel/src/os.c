164c8f1dfa070c55728387256a2c2852059acf8a
tracer-nju <tracer@nju.edu.cn>
2019-05-03 17:35:54 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 4168da8be8ec47a58693887131631321     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  17:35:54 up 16:02,  4 users,  load average: 0.47, 0.24, 0.19

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -39,7 +39,7 @@ void dummy_test(void* arg){
 	//printf("FA\n");
 }
 
-task_t* temp; 
+task_t* temp = pmm->alloc(sizeof(task_t));
 static void os_init() {
   LOG("os_init");
   pmm->init();
@@ -52,8 +52,7 @@ static void os_init() {
   kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty3");
   kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty4");*/
   kmt->create(pmm->alloc(sizeof(task_t)), "dummy1", dummy_test, (void*)1);
-  temp = pmm->alloc(sizeof(task_t));
-  kmt->create(temp, "dummy2", dummy_test, (void*)2);
+  kmt->create(pmm->alloc(sizeof(task_t)), "dummy2", dummy_test, (void*)2);
   #endif
 }
 
@@ -129,7 +128,7 @@ static _Context *os_trap(_Event ev, _Context *context) {
   
   //return context;
   printf("os_trap returns task: 0x%x\n",ret);
-  return &temp->context;
+  return ret;
 }
 
 
