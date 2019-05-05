5b9a6103e829629fd624d18191f540d9891fd858
tracer-nju <tracer@nju.edu.cn>
2019-05-05 22:25:11 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 4168da8be8ec47a58693887131631321     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  22:25:12 up 1 day,  3:53,  4 users,  load average: 0.28, 0.11, 0.03

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -83,7 +83,7 @@ int main(int argc, char *argv[]) {
     	sscanf(command,"%s",temp_buf);
     	int blank = 1;
     	for(int i = 0;i < strlen(temp_buf);i++){
-    		printf("%d",temp_buf[i]);
+    		printf("%d ",temp_buf[i]);
     		if(!isspace(temp_buf[i])){
     			blank = 0;
     			break;
