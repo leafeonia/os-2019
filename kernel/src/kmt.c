15889dc03502d0be8de0bdce42c523ca34cdde7f
tracer-nju <tracer@nju.edu.cn>
2019-05-06 14:10:33 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 6105548e88f44bd9820d7b1acdbc806b     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  14:10:33 up  4:28,  4 users,  load average: 0.09, 0.22, 0.33

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -232,7 +232,7 @@ static int kmt_create(task_t *task, const char *name, void (*entry)(void *arg), 
 	return 0;
 }
 
-void do_nothing(){while(1);}
+void do_nothing(){while(1){_yield()};}
 
 static void kmt_init(){
 	//LOG("kmt_init");
