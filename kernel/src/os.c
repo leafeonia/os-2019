8ba382fd52fb747aff2b6901332fb8c8786d987a
tracer-nju <tracer@nju.edu.cn>
2019-05-06 10:53:42 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 6105548e88f44bd9820d7b1acdbc806b     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  10:53:42 up  2:04,  4 users,  load average: 0.24, 0.20, 0.17

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -21,6 +21,7 @@ void echo_task(void *name){
 		char line[128], text[128];
 		sprintf(text, "(%s) $ ",name);
 		tty_write(tty, 0, text, strlen(name));
+		printf("checkpoint %s\n",name);
 		int nread = tty->ops->read(tty, 0, line, sizeof(line));
 		line[nread - 1] = '\0';
 		sprintf(text, "Echo %s.\n", line);
