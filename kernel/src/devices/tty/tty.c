68b297cdad13f64687548e785239fa1adca926ef
tracer-nju <tracer@nju.edu.cn>
2019-05-06 11:01:22 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 6105548e88f44bd9820d7b1acdbc806b     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  11:01:23 up  2:12,  4 users,  load average: 0.11, 0.09, 0.12

--- kernel/src/devices/tty/tty.c
+++ kernel/src/devices/tty/tty.c
@@ -208,10 +208,12 @@ ssize_t tty_read(device_t *dev, off_t offset, void *buf, size_t count) {
 ssize_t tty_write(device_t *dev, off_t offset, const void *buf, size_t count) {
   tty_t *tty = dev->ptr;
   kmt->sem_wait(&tty->lock);
+  LOG("cpt 1");
   for (size_t i = 0; i < count; i++) {
     tty_putc(tty, ((const char *)buf)[i]);
   }
   kmt->sem_signal(&tty->lock);
+  LOG("cpt 2");
   tty_render(tty);
   return count;
 }
