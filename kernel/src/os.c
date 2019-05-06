e360c50159fa5b47f0f4f20de15c6d47aa799301
tracer-nju <tracer@nju.edu.cn>
2019-05-06 14:18:37 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 6105548e88f44bd9820d7b1acdbc806b     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  14:18:38 up  4:36,  4 users,  load average: 0.52, 0.43, 0.37

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -142,7 +142,7 @@ static void os_run() {
 static _Context *os_trap(_Event ev, _Context *context) {
   kmt->spin_lock(&lk_trap);
   _Context* ret = context;
-  //if(ev.event != 5)printf("ev = %d\n",ev.event);
+  if(ev.event != 5)printf("ev = %d\n",ev.event);
   //printf("context->eip = 0x%x\n",context->eip);
   for(int i = 0;i < irq_id;i++){
     
