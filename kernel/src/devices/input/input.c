90a2908d5f944470af5ae80a9483c43f67736ac3
tracer-nju <tracer@nju.edu.cn>
2019-05-07 18:44:39 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 6105548e88f44bd9820d7b1acdbc806b     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  18:44:39 up 14:24,  6 users,  load average: 0.58, 0.80, 0.51

--- kernel/src/devices/input/input.c
+++ kernel/src/devices/input/input.c
@@ -80,7 +80,7 @@ void input_keydown(device_t *dev, int code) {
 }
 
 static _Context *input_notify(_Event ev, _Context *context) {
-  //printf("input notify\n");
+  LOG("input notify");
   kmt->sem_signal(&sem_kbdirq);
   return NULL;
 }
