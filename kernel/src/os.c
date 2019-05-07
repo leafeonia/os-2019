bd747bae8432faaa3f9807ee28c767a6dd2f988f
tracer-nju <tracer@nju.edu.cn>
2019-05-07 14:17:46 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 6105548e88f44bd9820d7b1acdbc806b     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  14:17:46 up 11:46,  6 users,  load average: 0.19, 0.09, 0.10

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -108,12 +108,12 @@ static void os_init() {
   //_vme_init(pmm->alloc,pmm->free);
   dev->init();
   #ifdef L2_TEST
-  kmt->create(pmm->alloc(sizeof(task_t)), "producer", producer, NULL);
-  kmt->create(pmm->alloc(sizeof(task_t)), "consumer", consumer, NULL);
-  /*kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty1");
+  //kmt->create(pmm->alloc(sizeof(task_t)), "producer", producer, NULL);
+  //kmt->create(pmm->alloc(sizeof(task_t)), "consumer", consumer, NULL);
+  kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty1");
   kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty2");
   kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty3");
-  kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty4");*/
+  kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty4");
   //printf("checkpoint 1 of os_init. intr_read = %d\n",_intr_read());
   /*kmt->create(pmm->alloc(sizeof(task_t)), "dummy1", dummy_test, (void*)1);
   kmt->create(pmm->alloc(sizeof(task_t)), "dummy2", dummy_test, (void*)2);
