ae0de217c33fe2d19773d16dfb74404df4dbbd74
tracer-nju <tracer@nju.edu.cn>
2019-05-09 13:48:21 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 151620f10d8c4ad39e239cb442f3df4e     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  13:48:21 up  6:26, 10 users,  load average: 2.29, 3.15, 3.13

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -1,4 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#define ERR(s)\
+	{printf("error: %s\n",s);\
+	exit(1);}\
+
 int main(int argc, char *argv[]) {
-    
+	void* p;
+	int fd = open(argv,O_RDONLY);
+	if(fd == -1){
+		ERR("open file failed");
+	}
+	struct stat st;
+	if(fstat(fd,&st) == -1){
+		close(fd);
+		ERR("obtain file size failed");
+	}    
+	int length = st.st_size;
+	p = mmap(NULL, length, PROT_READ, MAP_SHARED, fd, 0);
+	if(!p || p == (void*)-1){
+		close(fd);
+		ERR("mmap failed");
+	}
     return 0;
 }
