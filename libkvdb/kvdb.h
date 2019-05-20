274c1b8cff2637889d8deb71febbfb94f3fbfed1
tracer-nju <tracer@nju.edu.cn>
2019-05-20 21:25:24 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 70b0d7c0d3874adc993e977534a38100     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  21:25:24 up  4:16,  6 users,  load average: 1.29, 1.26, 1.51

--- libkvdb/kvdb.h
+++ libkvdb/kvdb.h
@@ -8,7 +8,7 @@ struct kvdb {
 typedef struct kvdb kvdb_t;  
  
 int kvdb_open(kvdb_t *db, const char *filename);
-int kvdb_close(kvdb_t *db); 
+int kvdb_close(kvdb_t *db);  
 int kvdb_put(kvdb_t *db, const char *key, const char *value);
 char *kvdb_get(kvdb_t *db, const char *key);
 
