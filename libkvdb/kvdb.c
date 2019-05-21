5ef81e5a94ce5d9a2b0f8f4f33ef182297868237
tracer-nju <tracer@nju.edu.cn>
2019-05-21 11:05:24 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 70b0d7c0d3874adc993e977534a38100     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  11:05:24 up  7:59,  7 users,  load average: 2.38, 2.35, 2.23

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -25,47 +25,49 @@ int kvdb_close(kvdb_t *db){
 	return 0;
 }
 int kvdb_put(kvdb_t *db, const char *key, const char *value){
-	printf("put~\n");
-	if(db->opened != 1){
-		printf("error: current kvdb has not successfully opened a db file yet\n");
-		return -1;
-	}
-	char temp[] = "temp.txt";
-	FILE* fp2 = fopen(temp,"w");
-	if(fp2 == NULL){
-		printf("error: create temporary file fails\n");
-		return -1;
-	}
-	FILE* fp = db->fp;
-	int matched = 0;
-	while(!feof(fp)){
-		char key_string[50000];
-		char value_string[50000];
-		fgets(key_string,50000,fp);
-		fgets(value_string,50000,fp);
-		if(strcmp(key, key_string) == 0){
-			matched = 1;
-			fprintf(fp2, "%s", key);
-			fprintf(fp2, "%s", value);
-		}
-		else{
-			fprintf(fp2, "%s", key_string);
-			fprintf(fp2, "%s", value_string);
-		}
-	}
-	if(!matched){
-		fprintf(fp2, "%s", key);
-		fprintf(fp2, "%s", value);
-	}
-	fclose(fp);
-	if(remove(db->filename) == -1){
-		printf("remove origin file fails\n");
-		fopen(db->filename,"w+");
-		return -1;
-	}
-	rename(temp,db->filename);
-	db->fp = fp2;
-	return 0;
+    printf("put~\n");
+    if(db->opened != 1){
+        printf("error: current kvdb has not successfully opened a db file yet\n");
+        return -1;
+    }
+    char temp[] = "temp.txt";
+    FILE* fp2 = fopen(temp,"w");
+    if(fp2 == NULL){
+        printf("error: create temporary file fails\n");
+        return -1;
+    }
+    FILE* fp = db->fp;
+    int matched = 0;
+    while(!feof(fp)){
+        char key_string[50000];
+        char value_string[50000];
+        fgets(key_string,50000,fp);
+        fgets(value_string,50000,fp);
+        if(feof(fp)) break;
+        if(strcmp(key, key_string) == 0){
+            matched = 1;
+            fprintf(fp2, "%s\n", key);
+            fprintf(fp2, "%s\n", value);
+        }
+        else{
+            fprintf(fp2, "%s", key_string);
+            fprintf(fp2, "%s", value_string);
+        }
+    }
+    if(!matched){
+        fprintf(fp2, "%s\n", key);
+        fprintf(fp2, "%s\n", value);
+    }
+    fclose(fp);
+    fclose(fp2);
+    if(remove(db->filename) == -1){
+        printf("remove origin file fails\n");
+        fopen(db->filename,"w+");
+        return -1;
+    }
+    rename(temp,db->filename);
+    db->fp = fopen(db->filename,"r+");
+    return 0;
 }
 
 char tmp[] = {'f','a'}; 
