9984924bcbe3bbe10dec572b40367c0b6fcdc8b2
tracer-nju <tracer@nju.edu.cn>
2019-05-21 10:28:03 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 70b0d7c0d3874adc993e977534a38100     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  10:28:03 up  7:22,  7 users,  load average: 2.17, 2.12, 2.21

--- libkvdb/test.c
+++ libkvdb/test.c
@@ -9,6 +9,7 @@ int main() {
 
   kvdb_open(&db, "a.db"); // BUG: should check for errors  
   kvdb_put(&db, key, "three-easy-pieces");
+  kvdb_put(&db, "leafeon", "470");
   value = kvdb_get(&db, key);
   kvdb_close(&db);
   printf("[%s]: [%s]\n", key, value);
