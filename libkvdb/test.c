43716851acb32434f59291f61ddeb3656cd10ec2
tracer-nju <tracer@nju.edu.cn>
2019-05-22 17:57:29 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 70b0d7c0d3874adc993e977534a38100     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  17:57:29 up 21:30,  7 users,  load average: 2.20, 2.41, 2.50

--- libkvdb/test.c
+++ libkvdb/test.c
@@ -9,13 +9,13 @@ void* test1(){
 	db.id = 1;
 	kvdb_open(&db, "a.db"); 
     kvdb_put(&db, "operating systems", "three-easy-pieces");
-    /*kvdb_put(&db, "glaceon", "471");
+    kvdb_put(&db, "glaceon", "471");
     kvdb_put(&db, "flareon", "136");
     kvdb_close(&db);
     kvdb_open(&db, "b.db");
     kvdb_put(&db, "eevee", "132");
     kvdb_put(&db, "vaporeon", "134");
-    kvdb_put(&db, "eevee", "133");*/
+    kvdb_put(&db, "eevee", "133");
     kvdb_close(&db);
 	return NULL;
 }
