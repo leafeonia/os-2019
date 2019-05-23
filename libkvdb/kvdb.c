a5e8abf21142721c1004aeed3af26c03741b99c0
tracer-nju <tracer@nju.edu.cn>
2019-05-23 15:43:09 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 70b0d7c0d3874adc993e977534a38100     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  15:43:10 up 1 day,  2:23,  7 users,  load average: 2.85, 2.82, 2.85

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -4,6 +4,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <sys/file.h>
+#include <sys/time.h>
 #include "kvdb.h"
 #include <errno.h>
 #include <time.h>
@@ -152,7 +153,9 @@ int kvdb_put(kvdb_t *db, const char *key, const char *value){
     //int fd = fileno(db->fp);
     //flock(fd,LOCK_EX);
     char temp[20];// = "temp.txt";
-    srand((unsigned)time(NULL));
+    struct timeval timee;
+    gettimeofday(&start, NULL);
+    srand(timee.tv_usec);
     sprintf(temp,"temp%d.txt",rand() % 100000);
     FILE* fptemp = fopen(db->filename,"r+");
     //printf("in put, opens %p, fd = %d\n",fptemp, fileno(fptemp));
