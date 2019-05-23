407237a7eb6265aa8facd39b9307f292a56abaf6
tracer-nju <tracer@nju.edu.cn>
2019-05-23 14:15:43 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 70b0d7c0d3874adc993e977534a38100     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  14:15:44 up 1 day, 56 min,  7 users,  load average: 3.16, 2.96, 2.37

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -192,15 +192,21 @@ int kvdb_put(kvdb_t *db, const char *key, const char *value){
         fprintf(fp2, "%s\n", key);
         fprintf(fp2, "%s\n", value);
     }
-    //printf("checkpoint\n");
     flock(fileno(fp),LOCK_UN);
+    fclose(fp);
+    rewind(fp2);
+    fp = fopen(db->filename, "w");
+    flock(fileno(fp),LOCK_EX);
+    
+    //printf("checkpoint\n");
+    /*flock(fileno(fp),LOCK_UN);
     flock(fileno(fp2),LOCK_UN);
     fclose(fp);
     fclose(fp2);
     fp = fopen(db->filename, "w");
     fp2 = fopen("temp.txt","r");
     flock(fileno(fp),LOCK_EX);
-    flock(fileno(fp2),LOCK_EX);
+    flock(fileno(fp2),LOCK_EX);*/
     while(!feof(fp2)){
     	//printf("meet again\n");
     	char key_string[130];
