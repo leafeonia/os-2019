e8837c85b5337f6fd4e9584f91ae732079003aa8
tracer-nju <tracer@nju.edu.cn>
2019-05-25 22:50:27 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 70b0d7c0d3874adc993e977534a38100     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  22:50:27 up 1 day,  7:35,  7 users,  load average: 2.53, 2.79, 2.85

--- libkvdb/boom-test.c
+++ libkvdb/boom-test.c
@@ -1,14 +1,15 @@
-
+#include <stdio.h>
+#include <kvdb.h>
 int main() {
   kvdb_t db;
   const char *key = "operating-systems";
   char *value1,*value2,*value3,*value4;
   kvdb_open(&db, "a.db"); // BUG: should check for errors  
   value1 = kvdb_get(&db, key);
-  value2 = kvdb_get(&db, "leafeon");
+  //value2 = kvdb_get(&db, "leafeon");
   value3 = kvdb_get(&db, "glaceon");
   kvdb_close(&db);
-  value4 = kvdb_get(&db, "leafeon");
+  //value4 = kvdb_get(&db, "leafeon");
   printf("[%s]: [%s]\n", key, value1);
   //printf("[%s]: [%s]\n", "leafeon", value2);
   printf("[%s]: [%s]\n", "glaceon", value3);
