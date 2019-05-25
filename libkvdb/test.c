1aec1f3ea6686f38ca631e3e67cd2b6c9452d115
tracer-nju <tracer@nju.edu.cn>
2019-05-25 23:41:14 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 70b0d7c0d3874adc993e977534a38100     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  23:41:14 up 1 day,  7:45,  7 users,  load average: 1.21, 1.82, 2.34

--- libkvdb/test.c
+++ libkvdb/test.c
@@ -46,7 +46,27 @@ void* test2(void* arg){
 }
 
 int main(int argc, char** argv) {
-	if(argc == 1) printf("bingo");
+	if(argc == 2) {
+      kvdb_t db;
+	  const char *key = "operating-systems";
+	  char *value1,*value3;
+	  kvdb_open(&db, "a.db"); // BUG: should check for errors  
+	  //kvdb_put(&db, key, "three-easy-pieces");
+	  //kvdb_put(&db, "leafeon", "470");
+	  //kvdb_put(&db, "leafeon", "471");
+	  value1 = kvdb_get(&db, key);
+	  //value2 = kvdb_get(&db, "leafeon");
+	  value3 = kvdb_get(&db, "glaceon");
+	  kvdb_close(&db);
+	  value4 = kvdb_get(&db, "leafeon");
+	  printf("[%s]: [%s]\n", key, value1);
+	  //printf("[%s]: [%s]\n", "leafeon", value2);
+	  printf("[%s]: [%s]\n", "glaceon", value3);
+	  //printf("[%s]: [%s]\n", "leafeon", value4);
+	  free(value1);
+	  free(value3);
+	  return 0;
+	}
   		
 	int rc = fork();
 	if(rc < 0) printf("error");
