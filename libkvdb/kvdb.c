afd97bb3eb177e24f3bed7f0dee62b0c0dbc2ef0
tracer-nju <tracer@nju.edu.cn>
2019-05-25 23:46:07 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 70b0d7c0d3874adc993e977534a38100     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  23:46:07 up 1 day,  7:50,  7 users,  load average: 1.68, 1.74, 2.16

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -365,7 +365,7 @@ char *kvdb_get(kvdb_t *db, const char *key){
     		if(buf2[strlen(buf2)-1] == '\n') buf2[strlen(buf2)-1] = '\0';
     		if(buf3[strlen(buf3)-1] == '\n') buf3[strlen(buf3)-1] = '\0';
     		//fscanf(fp,"%s %s %s",buf1,buf2,buf3);
-    		//printf("success. n = %lld.  %s %s %s\n",n,buf1,buf2,buf3);
+    		printf("success. n = %lld.  %s %s %s\n",n,buf1,buf2,buf3);
     		if(strcmp(buf1,key) == 0 && strcmp(buf3,"*") == 0){
     			free(buf1);
     			free(buf3);
