843b1b3cb76b398fa9fe7f07ced2b5596e53affa
tracer-nju <tracer@nju.edu.cn>
2019-05-30 19:50:12 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 979060c0d0e14423851ce1646e488180     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  19:50:12 up 49 min,  4 users,  load average: 3.02, 2.32, 1.18

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -21,10 +21,39 @@ struct semaphore {
 	struct spinlock lock;
 	int value;
 };
+struct inodeops {
+  int (*open)(file_t *file, int flags);
+  int (*close)(file_t *file);
+  ssize_t (*read)(file_t *file, char *buf, size_t size);
+  ssize_t (*write)(file_t *file, const char *buf, size_t size);
+  off_t (*lseek)(file_t *file, off_t offset, int whence);
+  int (*mkdir)(const char *name);
+  int (*rmdir)(const char *name);
+  int (*link)(const char *name, inode_t *inode);
+  int (*unlink)(const char *name);
+  // 你可以自己设计readdir的功能
+};
+
+
+
+struct inode {
+  int refcnt;
+  void *ptr;       // private data
+  struct filesystem *fs;
+  struct inodeops *ops; // 在inode被创建时，由文件系统的实现赋值
+                   // inode ops也是文件系统的一部分
+
+};
+
+struct file {
+  int refcnt; // 引用计数
+  struct inode *inode;
+  uint64_t offset;
+}
 
 struct fsops {
   void (*init)(struct filesystem *fs, const char *name, dev_t *dev);
-  inode_t *(*lookup)(struct filesystem *fs, const char *path, int flags);
+  struct inode *(*lookup)(struct filesystem *fs, const char *path, int flags);
   int (*close)(inode_t *inode);
 };
 
