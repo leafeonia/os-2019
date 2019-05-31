93dd45accb9abbc5d1a6036488ca547b4ffa66de
tracer-nju <tracer@nju.edu.cn>
2019-05-31 22:38:10 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 979060c0d0e14423851ce1646e488180     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  22:38:11 up  9:25,  4 users,  load average: 1.69, 2.07, 1.94

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -83,7 +83,7 @@ void dummy_test(void* arg){
 void fs(){
 	file_t* fs1fildes = (file_t*)(0xa3dc10);
 	for(int i = 0;i < NR_FILE;i++){
-		printf("%d:?%x %x?\n",i,(char*)fs1fildes+4*i,*((char*)fs1fildes+4*i));
+		printf("%d:?%x %x?\n",i,(char*)fs1fildes+4*i,*((file_t*)((char*)fs1fildes+4*i)));
 	}
 	vfs->open("/a.txt",1);
 	vfs->open("/proc",2);
