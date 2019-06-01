8e62f57badccaa53c436604d5913c89091b1d732
tracer-nju <tracer@nju.edu.cn>
2019-06-01 17:46:22 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 979060c0d0e14423851ce1646e488180     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  17:46:22 up 10:13,  6 users,  load average: 2.11, 2.37, 2.48

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -220,7 +220,7 @@ static int kmt_create(task_t *task, const char *name, void (*entry)(void *arg), 
 	}
 	printf("%s: %x %x\n",task->name, task, task->fildes);
 	for(int i = 3;i < NR_FILE;i++){
-		if(strcmp(name,"fs1") == 0)printf("%d:-%x %x-\n",i,&(task->fildes[i]),task->fildes[i]);
+		if(strcmp(name,"fs1") == 0)printf("%d:-%x %x- cpu:%d\n",i,&(task->fildes[i]),task->fildes[i], _cpu());
 	}
 	task->fence1 = MAGIC1;
 	task->fence2 = MAGIC2;
