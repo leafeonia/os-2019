5248678c8f5c881e491770c5cc52062ca9680c4a
tracer-nju <tracer@nju.edu.cn>
2019-06-03 09:20:15 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 979060c0d0e14423851ce1646e488180     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  09:20:16 up 17:03,  8 users,  load average: 2.06, 1.78, 1.96

--- kernel/src/devices/dev.c
+++ kernel/src/devices/dev.c
@@ -20,7 +20,7 @@ device_t *devices[0 DEVICES(DEV_CNT)];
 device_t *dev_lookup(const char *name) {
   for (int i = 0; i < LENGTH(devices); i++) 
     if (strcmp(devices[i]->name, name) == 0){
-    	//printf("dev find %d\n",i);
+    	printf("dev find %d, address: 0x%x\n",i, devices[i]);
     	return devices[i];
     }
       
