921894998d68bd05a89f443865eaf4a1bdfd93af
tracer-nju <tracer@nju.edu.cn>
2019-06-03 15:39:04 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 979060c0d0e14423851ce1646e488180     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  15:39:04 up 20:18,  8 users,  load average: 2.23, 1.96, 1.99

--- kernel/include/vfs.h
+++ kernel/include/vfs.h
@@ -1,7 +1,7 @@
 static filesystem_t* blkfs, *devfs, *procfs;
 static fsops_t* blkfs_ops, *devfs_ops, *procfs_ops;
-static inodeops_t* dev_inode_ops;//blk_inode_ops, *dev_inode_ops, *proc_inode_ops;
-static inode_t* devfs_inode[10];
+//static inodeops_t* blk_inode_ops, *dev_inode_ops, *proc_inode_ops;
+
 
 struct mount_point{
 	const char* path;
@@ -10,7 +10,7 @@ struct mount_point{
 static struct mount_point mt_list[5];
 static int mt_idx = 0;
 static spinlock_t lk_vfs;
-static spinlock_t lk_dev_inode_ops;
+
 
 
 //devfs
--- kernel/src/devfs.c
+++ kernel/src/devfs.c
@@ -1,6 +1,10 @@
 #include <common.h>
 #include <klib.h>
 
+static spinlock_t lk_dev_inode_ops;
+static inodeops_t* dev_inode_ops;
+static inode_t* devfs_inode[10];
+
 int boom(){
 	LOG("The file system doesn't support this function");
 	return -1;
