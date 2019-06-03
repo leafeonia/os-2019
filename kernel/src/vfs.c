a0cc29ecd58f2ed6181dc0841c7f4d6ba5c1c96c
tracer-nju <tracer@nju.edu.cn>
2019-06-03 09:29:46 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 979060c0d0e14423851ce1646e488180     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  09:29:46 up 17:12,  8 users,  load average: 1.30, 1.62, 1.83

--- kernel/src/vfs.c
+++ kernel/src/vfs.c
@@ -203,8 +203,9 @@ int vfs_open(const char *path, int flags){
 		}
 	}
 	const char* fs_path = path + omit;
-	//GOLDLOG(fs_path);
+	GOLDLOG(fs_path);
 	inode_t* inode = fs->ops->lookup(fs,fs_path,flags);
+	printf("inode->ptr = 0x%x\n",inode->ptr);
 	extern task_t** current_task[16];
 	task_t** cur = current_task[_cpu()];
 	int fd = -1;
