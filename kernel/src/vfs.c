bced701134cbb0623182cd75fa12aa97cdc7f3be
tracer-nju <tracer@nju.edu.cn>
2019-06-03 09:12:42 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 979060c0d0e14423851ce1646e488180     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  09:12:42 up 16:55,  8 users,  load average: 1.95, 1.83, 2.12

--- kernel/src/vfs.c
+++ kernel/src/vfs.c
@@ -53,7 +53,7 @@ int dev_inode_open(file_t *file, int flags, inode_t* inode){
 ssize_t dev_inode_write(file_t *file, const char *buf, size_t size){
 	LOG("YEAH");
 	device_t* dev = file->inode->ptr;
-	printf("inode = 0x%x, dev = 0x%x, dev->id = %d\n",file->inode, dev, dev->id);
+	printf("inode = 0x%x, dev = 0x%x, dev->name = %d\n",file->inode, dev, dev->name);
 	char text[] = "FAFAFAFAFAFAFA";
 	dev->ops->write(dev, 0, text, strlen(text));
 	return 0;
