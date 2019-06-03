21456b8a07a4a7b1570e8f5bc55b00d00df46efd
tracer-nju <tracer@nju.edu.cn>
2019-06-03 19:19:11 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 979060c0d0e14423851ce1646e488180     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  19:19:11 up 22:15,  8 users,  load average: 2.93, 2.26, 2.07

--- kernel/src/devfs.c
+++ kernel/src/devfs.c
@@ -31,7 +31,6 @@ ssize_t dev_inode_read(file_t *file, char *buf, size_t size){
 	device_t* dev = file->inode->ptr;
 	printf("name: %s\n",dev->name);
 	ssize_t nread = dev->ops->read(dev, file->offset, buf, size);
-		LOG("read2");
 	file->offset += size;
 	return nread;
 }
