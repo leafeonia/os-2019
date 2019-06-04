ac39a8ce4121af38a634e19ca7a3c6d1af21d79e
tracer-nju <tracer@nju.edu.cn>
2019-06-04 18:05:19 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 979060c0d0e14423851ce1646e488180     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  18:05:19 up 1 day,  7:29,  6 users,  load average: 2.21, 2.34, 2.46

--- kernel/src/blkfs.c
+++ kernel/src/blkfs.c
@@ -113,6 +113,7 @@ int get_available_inode(){
 		if(inodes[i].refcnt == 0) {
 			inodes[i].refcnt = 1;
 			inodes[i].block[0] = get_available_data_block();
+			blkfs->dev->ops->write(blkfs->dev, 0, &inodes, BLOCK_SIZE);
 			GOLDLOG("get available inode #%d",i);
 			return i;
 		}
