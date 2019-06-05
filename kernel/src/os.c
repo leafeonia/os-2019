e3b1764b1a048a8f2bfa8b5f68513e5967d4fc96
tracer-nju <tracer@nju.edu.cn>
2019-06-05 15:22:17 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 979060c0d0e14423851ce1646e488180     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  15:22:17 up 1 day, 11:38,  6 users,  load average: 3.54, 2.20, 1.76

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -161,14 +161,11 @@ static void shell(void* name){
   sprintf(pwd,"/");
   while (1) {
     //char input[128], output[512];
-    CYANLOG("nya~");
     sprintf(output, "(%s) $ ", name); 
     //tty->ops->write(tty, 0, output, strlen(output));
     //LOG("FA1");
     vfs->write(stdout, output, sizeof(output));
-    CYANLOG("nya2~");
     int nread = vfs->read(stdin, input, sizeof(input));
-    CYANLOG("nya3~");
     input[nread - 1] = '\0';
     if(strcmp("ls",input) == 0){
     	ls(output, pwd);
