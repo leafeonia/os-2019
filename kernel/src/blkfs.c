39d0f9a6c0da06bf6e3d7abc8f583f96d15df579
tracer-nju <tracer@nju.edu.cn>
2019-06-06 20:03:58 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 979060c0d0e14423851ce1646e488180     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  20:03:58 up 1 day, 20:03,  6 users,  load average: 1.85, 2.16, 1.96

--- kernel/src/blkfs.c
+++ kernel/src/blkfs.c
@@ -70,9 +70,10 @@ int blk_inode_link(const char *name, inode_t *inode){
 			cur_path[i] = *(left_path + i);
 		}
 		blkfs->dev->ops->read(blkfs->dev, DATA(get_data_offset(inode_id)), dir, BLOCK_SIZE);
+		GOLDLOG("enter1");
 		for(int i = 0;i < NR_DIRE;i++){
 			if(i == NR_DIRE){
-				GOLDLOG("enter");
+				GOLDLOG("enter2");
 					for(int j = 0;j < strlen(left_path);j++){
 						if(*(left_path + j) == '/'){
 							LOG("error: create file in non-existing directory. Please use mkdir to create the directory first.");
