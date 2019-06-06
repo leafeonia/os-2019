40f003d8a8c40b39a1cc99093eac7437d833aa35
tracer-nju <tracer@nju.edu.cn>
2019-06-06 21:01:20 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 979060c0d0e14423851ce1646e488180     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  21:01:20 up 1 day, 21:00,  6 users,  load average: 1.54, 2.06, 2.01

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -226,7 +226,7 @@ static void shell(void* name){
     
     //touch
     else if(strncmp("touch ",input,6) == 0){
-    	sprintf(output,"");
+    	sprintf(output,"\0");
     	char* newfile = input + 6;
     	while(*newfile == ' ') newfile++; //remove blank
     	if(strlen(newfile) == 0) sprintf(output,"touch: please type in filename\n");
