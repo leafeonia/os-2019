d2b26d450fe26f9670f280c7d9e6618db0e0ef34
tracer-nju <tracer@nju.edu.cn>
2019-06-06 18:49:50 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 979060c0d0e14423851ce1646e488180     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  18:49:50 up 1 day, 18:49,  6 users,  load average: 1.71, 1.86, 1.57

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -196,7 +196,7 @@ static void link(char* output, char* pwd, char* oldpath, char* newpath){
 		sprintf(absolute_oldpath,"%s/%s",pwd, oldpath);
 	}
 	if(vfs->link(absolute_oldpath, absolute_newpath)) sprintf(output, "link fails");
-	sprintf(output,"\n");
+	//sprintf(output,"\n");
 }
 
 static void shell(void* name){
