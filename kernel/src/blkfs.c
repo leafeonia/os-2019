09634175553fc785ed51d17279be9e6c132adc9c
tracer-nju <tracer@nju.edu.cn>
2019-06-07 20:17:02 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 979060c0d0e14423851ce1646e488180     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  20:17:02 up 2 days,  1:58,  6 users,  load average: 2.89, 2.63, 2.56

--- kernel/src/blkfs.c
+++ kernel/src/blkfs.c
@@ -167,13 +167,28 @@ int blk_inode_link(const char *name, inode_t *inode){
 }*/
 
 int blk_inode_mkdir(const char *name, inode_t* inode){
+	/*find parent inode_id*/
+	int parent_inode_id = 2;
+	int le = strlen(name) - 1;
+	while(le > 0 && *(name + le) != '/') le--;
+	if(le == 0) parent_inode_id = 2;//root
+	else{
+		char parent_path[128];
+		strcpy(parent_path, name);
+		parent_path[le] = '\0';
+		inode_t* parent = blkfsops_lookup(blkfs, parent_path, 0);
+		parent_inode_id = get_inode_id(parent);
+		CYANLOG("parent_inode_id = %d",parent_inode_id);
+	}
+	
+	
 	dire_t dir[NR_DIRE];
 	memset(dir,0,sizeof(dir));
 	//blkfs->dev->ops->read(blkfs->dev, DATA(inode->block[0]), &dir, BLOCK_SIZE);
 	strcpy(dir[0].name,".");
 	dir[0].inode_id = get_inode_id(inode);
 	strcpy(dir[1].name,"..");
-	dir[1].inode_id = ROOT;
+	dir[1].inode_id = parent_inode_id;
 	//strcpy(dir[2].name,"test.txt");
 	//dir[2].inode_id = get_available_inode();
 	//LOG("%d",inode->block[0]);
