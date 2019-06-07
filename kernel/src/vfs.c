001786419819a264b41b321ec1ad31a7e4caaec2
tracer-nju <tracer@nju.edu.cn>
2019-06-07 15:58:22 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 979060c0d0e14423851ce1646e488180     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  15:58:23 up 2 days, 5 min,  6 users,  load average: 2.61, 2.78, 2.66

--- kernel/src/vfs.c
+++ kernel/src/vfs.c
@@ -165,6 +165,7 @@ int vfs_open(const char *path, int flags){
 	const char* fs_path = findfs(path,&fs);
 	CYANLOG("filesystem %s opens %s",fs.name,fs_path);
 	inode_t* inode = fs.ops->lookup(&fs,fs_path,flags);
+	LOG("%d",inode->block[0]);
 	if(!inode){
 		LOG("vfs->open(%s, %d) fails", path, flags);
 		//kmt->spin_unlock(&lk_vfs);
