23e64be4959ab7fb8bf2356b3a21eeb1ae9c4c71
tracer-nju <tracer@nju.edu.cn>
2019-06-07 21:48:44 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 979060c0d0e14423851ce1646e488180     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  21:48:44 up 2 days,  3:29,  6 users,  load average: 1.06, 0.94, 0.94

--- kernel/src/blkfs.c
+++ kernel/src/blkfs.c
@@ -209,7 +209,9 @@ int blk_inode_unlink(const char* name){
 		parent_path[le] = '\0';
 	}
 	inode_t* parent = blkfsops_lookup(blkfs, parent_path, 0);
+	LOG("?");
 	if(!parent) return -1;
+	LOG("???");
 	//CYANLOG("parent->block[0] = %d",parent->block[0]);
 	remove_file = name + le + 1;
 	if(strcmp(remove_file,".") == 0 || strcmp(remove_file,"..") == 0){
