0257aea6dc294ada8604217cc3ed040f4ab12dab
tracer-nju <tracer@nju.edu.cn>
2019-06-07 20:21:46 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 979060c0d0e14423851ce1646e488180     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  20:21:46 up 2 days,  2:02,  6 users,  load average: 2.91, 2.75, 2.63

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -242,7 +242,7 @@ static void shell(void* name){
     //char input[128], output[512];
     memset(input,0,sizeof(input));
     memset(output, 0, sizeof(output));
-    sprintf(output, "(%s) %s $ ",pwd, name); 
+    sprintf(output, "(%s) %s $ ", name, pwd); 
     //tty->ops->write(tty, 0, output, strlen(output));
     //LOG("FA1");
     vfs->write(stdout, output, sizeof(output));
