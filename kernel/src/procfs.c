0f96ca2d9442190ae6ff93fc974dcebb3eaa1453
tracer-nju <tracer@nju.edu.cn>
2019-06-08 14:09:39 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 979060c0d0e14423851ce1646e488180     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  14:09:39 up 2 days,  8:13,  7 users,  load average: 0.93, 1.03, 0.99

--- kernel/src/procfs.c
+++ kernel/src/procfs.c
@@ -1,9 +1,43 @@
 #include <common.h>
 #include "vfs.h"
+#include <klib.h>
+#include <devices.h>
+#include "my_os.h"
+
+static inodeops_t* proc_inode_ops;
+static inode_t* procfs_inode[10];
+
+int boom(){
+	LOG("procfs doesn't support this function");
+	return -1;
+}
+
+static int dummy(){return 0;}
 
 void procfs_init(filesystem_t *fs, const char *name, device_t *dev){
 	fs->name = name;
 	fs->dev = dev;	
 	procfs_ops = pmm->alloc(sizeof(fsops_t));
 	procfs->ops = procfs_ops;
+	extern task_t* tasks[16][NR_TASK];
+	for(int i = 0;i < 16;i++){
+		for(int j = 0;j < NR_TASK;j++){
+			task_t* cur = tasks[i][j];
+			if(cur->name) GOLDLOG(cur->name);
+		}
+	}
+
+	
+	
+	
+	proc_inode_ops = pmm->alloc(sizeof(inodeops_t));
+	proc_inode_ops->open   = proc_inode_open;
+	proc_inode_ops->close  = proc_inode_close;
+	proc_inode_ops->read   = proc_inode_read;
+	proc_inode_ops->write  = proc_inode_write;
+	proc_inode_ops->lseek  = dummy;
+	proc_inode_ops->mkdir  = boom;
+	proc_inode_ops->rmdir  = boom;
+	proc_inode_ops->link   = boom;
+	proc_inode_ops->unlink = boom;
 }
