60cbcf9571d920e1e515ef0d92c4ae9d9c4ba8a4
tracer-nju <tracer@nju.edu.cn>
2019-06-08 15:27:38 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f378d9f9c66a47178504795353170859            Boot ID: 979060c0d0e14423851ce1646e488180     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  15:27:38 up 2 days,  9:31,  7 users,  load average: 0.19, 0.10, 0.14

--- kernel/src/procfs.c
+++ kernel/src/procfs.c
@@ -4,8 +4,8 @@
 #include <devices.h>
 #include "my_os.h"
 
-/*static inodeops_t* proc_inode_ops;
-static inode_t* procfs_inode[10];
+static inodeops_t* proc_inode_ops;
+static inode_t procfs_inode;
 
 int boom(){
 	LOG("procfs doesn't support this function");
@@ -13,11 +13,24 @@ int boom(){
 }
 
 static int dummy(){return 0;}
-*/
+static int dummy2(){}
+
 inode_t* procfsops_lookup(filesystem_t *fs, const char *path, int flags){
 	GOLDLOG("procfsops_lookup: path = %s",path);
-	//if(strcmp(path,"/.") == 0) 
-	return NULL;
+	const char* left_path = path + 1;
+	if(strcmp(left_path,".") == 0) procfs_inode.block[0] = 1;
+	else if(strcmp(left_path,"cpuinfo") == 0) procfs_inode.block[0] = 2;
+	else if(strcmp(left_path,"meminfo") == 0) procfs_inode.block[0] = 3;
+	return &procfs_inode;
+}
+
+ssize_t proc_inode_read(file_t *file, char *buf, size_t size){
+	int code = file->inode->block[0];
+	if(code == 1) sprintf(buf, "ls of procfs");
+	else if(code == 2) sprintf(buf, "cpuinfo: %d",_ncpu());
+	else if(code == 3) sprintf(buf, "meminfo");
+	file->offset += size;
+	return size;
 }
 
 void procfs_init(filesystem_t *fs, const char *name, device_t *dev){
@@ -33,19 +46,25 @@ void procfs_init(filesystem_t *fs, const char *name, device_t *dev){
 		}
 	}
 	
+	procfs_ops->init   = dummy2;
 	procfs_ops->lookup = procfsops_lookup;
+	procfs_ops->close  = dummy;
 
+	procfs_inode.ptr = NULL;
+	procfs_inode.refcnt = 0;
+	procfs_inode.fs = procfs;
+	procfs_inode.ops = procfs_inode_ops;
+	for(int i = 0;i < 12;i++) procfs_inode.block[i] = 0;
 	
 	
-	/*
 	proc_inode_ops = pmm->alloc(sizeof(inodeops_t));
-	proc_inode_ops->open   = proc_inode_open;
+	/*proc_inode_ops->open   = proc_inode_open;
 	proc_inode_ops->close  = proc_inode_close;
 	proc_inode_ops->read   = proc_inode_read;
-	proc_inode_ops->write  = proc_inode_write;
+	proc_inode_ops->write  = proc_inode_write;*/
 	proc_inode_ops->lseek  = dummy;
 	proc_inode_ops->mkdir  = boom;
 	proc_inode_ops->rmdir  = boom;
 	proc_inode_ops->link   = boom;
-	proc_inode_ops->unlink = boom;*/
+	proc_inode_ops->unlink = boom;
 }
